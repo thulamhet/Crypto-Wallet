bd72f07a1ffc67174155f77518639b7021c58c75
Ha Do <hadth89@gmail.com>
2021-08-19 09:29:47 +0700

    Move assets folder

--- src/themes/icons.ts
+++ src/themes/icons.ts
@@ -1,9 +1,9 @@
   
-export const menu = require("C:/Users/thu dep trai/CryptoWallet/assets/icons/menu_icon.png");
-export const search = require("C:/Users/thu dep trai/CryptoWallet/assets/icons/search_icon.png");
-export const bitcoin = require("C:/Users/thu dep trai/CryptoWallet/assets/icons/bitcoin_icon.png");
-export const ethereum = require("C:/Users/thu dep trai/CryptoWallet/assets/icons/ethereum_icon.png");
-export const tether = require("C:/Users/thu dep trai/CryptoWallet/assets/icons/tether_icon.png");
+export const menu = require("../assets/icons/menu_icon.png");
+export const search = require("../assets/icons/search_icon.png");
+export const bitcoin = require("../assets/icons/bitcoin_icon.png");
+export const ethereum = require("../assets/icons/ethereum_icon.png");
+export const tether = require("../assets/icons/tether_icon.png");
 
 export default {
     menu,
